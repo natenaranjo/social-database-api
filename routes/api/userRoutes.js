a8623b8109cbc45efa8e897b965597f8c3b255c0
Nate Naranjo <naranjo_nathaniel@live.com>
2022-10-29 08:17:18 -0500

    Update the User Route to add the ability to add and remove friends

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,10 +5,27 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    addUserFriend,
+    removeUserFriend,
 } = require('../../controllers/userController');
 
-router.route('/').get(getUsers).post(createUser);
+router
+    .route('/')
+    .get(getUsers)
+    .post(createUser);
 
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route('/:userId/friends/')
+    .post(addUserFriend);
+
+router
+    .route('/:userId/friends/:friendId')
+    .delete(removeUserFriend);
 
 module.exports = router;
